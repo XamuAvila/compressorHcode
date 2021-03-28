cd895c03d2760975e0e5024b2aed32090a5cdd4d
Samuel Ávila Fernandes <samuel.avila25099@gmail.com>
2021-03-28 10:44:54 -0300

    Compressor de Vìdeo

---
+++ videos.js
@@ -0,0 +1,65 @@
+const { spawn } = require('child_process');
+const parent = process.argv[2];//Pegar o parâmetro de onde esta sendo chamado
+let videos = [];
+
+if (process.argv[2]) {
+    const start = parseInt(process.argv[3]);
+    const end = parseInt(process.argv[4]);
+
+    for (let i = start; i <= end; i++) {
+        videos.push(i);
+    }
+    videos.reverse();
+    processVideo();
+}else{
+    console.log('é necessário criar um diretório de nível superior');
+}
+
+function resize(video, quality) {
+    const p = new Promise((resolve, reject) => {
+        const ffmpeg = spawn('./ffmpeg/bin/ffmpeg',[
+            '-i',
+            `${parent}/${video}.mp4`,
+            '-codec:v',
+            'libx264',
+            '-profile:v',
+            'main',
+            '-preset',
+            'slow',
+            '-b:v',
+            '400k',
+            '-maxrate',
+            '400k',
+            '-bufsize',
+            '800k',
+            '-vf',
+            `scale=-2:${quality}`,
+            '-threads',
+            '0',
+            '-b:a',
+            '128k',
+            `${parent}/resultado/${video}-${quality}.mp4`
+        ])
+        ffmpeg.stderr.on('data', (data) => {
+            console.log(data);
+        })
+        ffmpeg.on('close', (close) => {
+            resolve();
+        })
+    });
+}
+
+async function processVideo(){
+    let video = videos.pop(); //remove um elemento retornando ele
+    if(video){
+        try {
+            await resize(video, 720);
+            await resize(video, 480);
+            await resize(video, 360);
+            console.log('videos renderizados - ', video );
+            processVideo();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
